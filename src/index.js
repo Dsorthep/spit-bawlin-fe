8e574dcfec77d99aac7880b5fe466fd0aa9013dc
dsorthep <detvisiths@yahoo.com>
2020-02-03 15:42:55 -0600

    set up basic front end with health check

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from "axios";
+
+// Health Check
+axios.get("http://localhost:5000/ping").then(() => {
+    console.log("Should be working")
+}).catch(() => {
+    console.log("Not working");
+})
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
