409f068b5bbf0c79f1a412820e64827dfef365f8
dsorthep <detvisiths@yahoo.com>
2020-02-05 13:24:09 -0600

    styles for main bulletin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React from 'react';
-// import NoteCard from "./NoteCard";
+import NoteCard from "./NoteCard";
 import './App.css';
 import "bulma/css/bulma.css";
+// import * as Coffeehouse from "./coffeehouses.jpg";
 
 
 export default function App() {
   return (
     <div className="App">
-      <h1 className="title">Spit Bawlin</h1>
-      {/* <textarea className="memo" placeholder="What's on your mind?"></textarea> */}
+      <h1 className="brand">Spit Bawlin</h1>
+      <textarea className="memo" placeholder="What's on your mind?"></textarea>
       <br></br>
-      {/* <button className="button">CREATE A MEMO</button> */}
-      {/* <NoteCard /> */}
+      <button className="button">CREATE A MEMO</button>
+      <br></br>
+      <NoteCard />
+      <div class="tabs is-toggle is-fullwidth is-large">
+  <ul>
+    <li class="resources-menu">
+      <a href="https://www.javascript.com/">
+        <span class="icon"><i class="fab fa-js-square" aria-hidden="true"></i></span>
+        <span>JavaScript Resources</span>
+      </a>
+    </li>
+    <li class="resources-menu">
+      <a href="https://reactjs.org/">
+        <span class="icon"><i class="fab fa-react" aria-hidden="true"></i></span>
+        <span>ReactJS Resources</span>
+      </a>
+    </li>
+    <li class="resources-menu">
+      <a href="https://www.w3schools.com/html/html_css.asp">
+        <span class="icon"><i class="fas fa-file-code" aria-hidden="true"></i></span>
+        <span>HTML/CSS Resources</span>
+      </a>
+    </li>
+  </ul>
+</div>
+
+
       </div>
       
       
--- src/NoteCard.js
+++ src/NoteCard.js
@@ -1,24 +1,37 @@
 import React from "react";
 import "bulma/css/bulma.css";
-import PostIt from "./post-it.jpg";
+// import PostIt from "./post-it.jpg";
 
-// export default function  NoteCard() {
-//     return (
-        
-//         <div class="modal">
-//             <image src={PostIt}></image>
-//       <div class="modal-background"></div>
-//       <div class="modal-card">
-//         <header class="modal-card-head">
-//           <p class="modal-card-title">My Notes</p>
-//           <button class="delete" aria-label="close"></button>
-//         </header>
-//         <section class="modal-card-body">
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-//         </section>
-//         <footer class="modal-card-foot">
-//         </footer>
-//       </div>
-//     </div>
-//     )
-// }
+export default function  NoteCard() {
+    return (
+      <div className="tile is-ancestor">
+  <div className="tile is-parent">
+    <article className="tile is-child box">
+      <p className="title">TEST!!!</p>
+      <p className="subtitle">Thursday, Feb 6</p>
+    </article>
+  </div>
+  <div className="tile is-parent">
+    <article className="tile is-child box">
+      <p className="title">Job Interview</p>
+      <p className="subtitle">@Target: Wednesday, Feb 5</p>
+    </article>
+  </div>
+  <div className="tile is-parent">
+    <article className="tile is-child box">
+      <p className="title">Jr. Dev Meetup</p>
+      <p className="subtitle">Friday, Feb  7</p>
+      <div className="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+      </div>
+    </article>
+  </div>
+  <div className="tile is-parent">
+    <article className="tile is-child box">
+      <p className="title">Job Interview</p>
+      <p className="subtitle">@General Mills: Wednesday, Feb 12</p>
+    </article>
+  </div>
+</div>
+    )
+  }
