9eddedec3dce32f3e92309f826e873d81201a87e
dsorthep <detvisiths@yahoo.com>
2020-02-06 11:02:50 -0600

    added resources component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NoteForm from "./components/NoteForm";
 import NoteCard from "./components/NoteCard";
+import Resources from "./components/Resources";
 import Coffee from "./components/Coffee";
 import './App.css';
 import "bulma/css/bulma.css";
@@ -15,28 +16,8 @@ export default function App() {
       <NoteForm />
       <br></br>
       <NoteCard />
-      <div class="tabs is-toggle is-fullwidth is-large">
-  <ul>
-    <li class="resources-menu">
-      <a href="https://www.javascript.com/">
-        <span class="icon"><i class="fab fa-js-square" aria-hidden="true"></i></span>
-        <span>JavaScript Resources</span>
-      </a>
-    </li>
-    <li class="resources-menu">
-      <a href="https://reactjs.org/">
-        <span class="icon"><i class="fab fa-react" aria-hidden="true"></i></span>
-        <span>ReactJS Resources</span>
-      </a>
-    </li>
-    <li class="resources-menu">
-      <a href="https://www.w3schools.com/html/html_css.asp">
-        <span class="icon"><i class="fas fa-file-code" aria-hidden="true"></i></span>
-        <span>HTML/CSS Resources</span>
-      </a>
-    </li>
-  </ul>
-</div>
+      <br></br>
+      <Resources />
 <br></br>
 <Coffee />
 
