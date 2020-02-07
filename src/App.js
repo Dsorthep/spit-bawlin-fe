41752b139d036c4d3cbb6bda307e09f1e185087a
dsorthep <detvisiths@yahoo.com>
2020-02-07 13:53:22 -0600

    setting up adding a note

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NoteForm from "./components/NoteForm";
-import NoteCard from "./components/NoteCard";
+// import NoteCard from "./components/NoteCard";
 import Resources from "./components/Resources";
 import Coffee from "./components/Coffee";
 
@@ -9,16 +9,42 @@ import "bulma/css/bulma.css";
 // import * as Coffeehouse from "./coffeehouses.jpg";
 
 
-export default function App() {
+export default function App(noteTitle, noteDate, noteText) {
+  const [notes, setNotes] = useState([])
+
+  function addNote(note) {
+    setNotes([...notes, note])
+  }
+
   return (
     <div className="App">
       <h1 className="brand">Spit Bawlin</h1>
       <br></br>
-      <div className="columns form-coffee"><NoteForm />
+      <div className="columns form-coffee">
+      <NoteForm addNote={addNote}/>
       <Coffee />
       </div>
       <br></br>
-      <NoteCard />
+      {notes.map(note => (
+        <div className="tile is-parent">
+        <article className="tile is-child box">
+        
+          <p className="title">{noteTitle}</p>
+          
+        
+        
+          
+          <p className="subtitle">{noteDate}</p>
+      
+         <br></br>
+         
+           
+          <p>{noteText}</p>
+         
+        
+        </article>
+      </div>
+      ))}
       <br></br>
       <Resources />
       
--- src/NoteCard.js
+++ src/NoteCard.js
@@ -3,40 +3,39 @@ import "bulma/css/bulma.css";
 import NoteForm from "./components/NoteForm";
 // import PostIt from "./post-it.jpg";
 
-export default function  NoteCard({noteTitle, noteDate, noteText}) {
+export default function  NoteCard() {
+    // const [noteTitle, setNoteTitle] = useState("");
+    // const [noteDate, setNoteDate] = useState("");
+    // const [noteText, setNoteText] = useState("");
     return (
-      <NoteForm>
-      {/* <div className="tile is-ancestor"> */}
-  <div className="tile is-parent">
-    <article className="tile is-child box">
-      <p className="title">{noteTitle}</p>
-      <p className="subtitle">{noteDate}</p>
-      <br></br>
-    <p>{noteText}</p>
-    </article>
-  </div>
-  </NoteForm>
-  /* <div className="tile is-parent">
-    <article className="tile is-child box">
-      <p className="title">Job Interview</p>
-      <p className="subtitle">@Target: Wednesday, Feb 5</p>
-    </article>
-  </div>
-  <div className="tile is-parent">
-    <article className="tile is-child box">
-      <p className="title">Jr. Dev Meetup</p>
-      <p className="subtitle">Friday, Feb  7</p>
-      <div className="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-      </div>
-    </article>
-  </div>
-  <div className="tile is-parent">
-    <article className="tile is-child box">
-      <p className="title">Job Interview</p>
-      <p className="subtitle">@General Mills: Wednesday, Feb 12</p>
-    </article>
-  </div> */
-// </div>
-    )
+      
+      <NoteForm />
+  //     handleFormSubmit={({ preventDefault }) => {
+  //       preventDefault();
+  //     }}
+  //     handleTitleChange={({ target }) => setNoteTitle(target.value)}
+  //     handleDateChange={({ target }) => setNoteDate(target.value)}
+  //     handleTextChange={({ target }) => setNoteText(target.value)} />
+     
+  // <div className="tile is-parent">
+  //   <article className="tile is-child box">
+  //   {noteTitle.map(noteTitle => (
+     
+  //     <p className="title">{noteTitle}</p>
+      
+  //   ))}
+  //   {noteDate.map(noteDate => (
+      
+  //     <p className="subtitle">{noteDate}</p>
+  //   ))}
+  //    <br></br>
+  //    {noteText.map(noteText => (
+       
+  //     <p>{noteText}</p>
+  //    ))}
+    
+  //   </article>
+  // </div>
+  
+    );
   }
