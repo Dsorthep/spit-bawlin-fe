1f0df4b0387dfd75013f7d848f0923ce059476d5
dsorthep <detvisiths@yahoo.com>
2020-02-09 20:42:29 -0600

    resources

--- src/utils/initialState.js
+++ src/utils/initialState.js
@@ -1,3 +1,4 @@
 export const initialState = {
-    notes: []
+    notes: [],
+    resources: []
   };
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -7,6 +7,11 @@ export const reducer = (state, action) => {
         ...state,
         notes: [...state.notes, action.payload]
       };
+    case CONSTANTS.APPEND_RESOURCE:
+      return {
+        ...state,
+        resources: [...state.resources, action.payload]
+      };
     default:
       break;
   }
