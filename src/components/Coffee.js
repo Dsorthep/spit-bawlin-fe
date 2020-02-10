a55b1577fb757f133dca3692dc074f0e5980830e
dsorthep <detvisiths@yahoo.com>
2020-02-10 12:20:12 -0600

    things

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -1,18 +1,13 @@
 import React from "react";
 // import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 import "bulma/css/bulma.css";
-import {GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps";
-
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 function Map(){
     return <GoogleMap 
     defaultZoom={10} 
     defaultCenter={{lat: 44.943722, lng: -93.094276}} />;
 }
-
 const WrappedMap = withScriptjs(withGoogleMap(Map));
-
-
-
 export default function Coffee() {
   return (
     <div className="coffee-map font-effect-neon">
@@ -22,12 +17,23 @@ export default function Coffee() {
         loadingElement={<div style={{height: "100%"}}/>}
         containerElement={<div style={{height: "100%"}}/>}
         mapElement={<div style={{height: "100%"}}/>}
-
-
-
-
       />
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
